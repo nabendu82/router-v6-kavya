153bce44c0acfeba566843ea7b10f3da4da8ae95
nabendu82 <nabendu.biswas@gmail.com>
2024-07-19 07:36:01 +0530

    first commit

---
+++ src/components/AllCourses.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AllCourses = () => {
+  return (
+    <div className='container'>
+        <h2>All Courses</h2>
+        <ul className="list-group mb-2">
+            <li className="list-group-item">HTML</li>
+            <li className="list-group-item">CSS</li>
+            <li className="list-group-item">JavaScript</li>
+            <li className="list-group-item">ReactJS</li>
+            <li className="list-group-item">Angular</li>
+        </ul>
+    </div>
+  )
+}
+
+export default AllCourses
